e968eb044ec88a40be037e41ec86b8ad8afb52f4
Alien-web <surumypallickal@gmail.com>
2021-05-25 22:18:05 +0530

    nav item selection completed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  active:string="login";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  activate(string:string){
+   this.active=string;
+  }
 }
